720aa0953816770b17976a6dc68a81a5a3b0649b
Beny <benynouba@gmail.com>
2026-03-05 15:59:04 +0100

    login

---
+++ src/main/java/com/example/myhotel/dtos/Requests/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.myhotel.dtos.Requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String Username;
+    private String password;
+
+}
