8f222e3558a5b1cd97028a8caa2d24fceebcc56a
Beny <benynouba@gmail.com>
2026-04-04 07:39:49 +0100

    security

--- src/main/java/com/example/myhotel/config/SecurityConfig.java
+++ src/main/java/com/example/myhotel/config/SecurityConfig.java
@@ -3,17 +3,38 @@ package com.example.myhotel.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+        http.csrf(CsrfConfigurer::disable); // csrf -> csrf.disable()
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/api/v1/hotel/payment").permitAll()
+                .anyRequest().authenticated()
+
+        );
         http.formLogin(formLogin -> formLogin.disable());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
+
+
+/*
+
+HTTP -> FilterChain -> Authentication Manager -> Authentication Provider -> Authentication -> UserDetailsService -> UsernamePassword...
+
+
+
+
+
+ */
+
+
+
+
